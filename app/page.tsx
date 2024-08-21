444987dfdc1deef65a0c7322f1c9b84f9ebbe2b5
Théo Dufort <theodufort05@gmail.com>
2024-08-21 10:20:36 -0400

    update

--- app/page.tsx
+++ app/page.tsx
@@ -7,10 +7,32 @@ import Footer from "@/components/Footer";
 import Hero from "@/components/Hero";
 import Pricing from "@/components/Pricing";
 import Problem from "@/components/Problem";
-import { Suspense } from "react";
+import { Suspense, useEffect, useState } from "react";
 import BookFinder from "@/components/BookFinder";
+import { Database } from "@/types/supabase";
+import {
+  createClientComponentClient,
+  User,
+} from "@supabase/auth-helpers-nextjs";
+import { useRouter } from "next/router";
 
 export default function Page() {
+  const router = useRouter();
+  const supabase = createClientComponentClient<Database>();
+  const [user, setUser] = useState<User | null>(null);
+  useEffect(() => {
+    const getUser = async () => {
+      const { data } = await supabase.auth.getUser();
+      setUser(data.user);
+    };
+
+    getUser();
+  }, [supabase]);
+  useEffect(() => {
+    if (user) {
+      router.push("/dashboard");
+    }
+  }, [user]);
   return (
     <>
       <Suspense>
