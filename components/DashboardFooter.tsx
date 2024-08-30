3696e4951002b8e22dc09d5d1e49d3c0a3734956
Théo Dufort <theodufort05@gmail.com>
2024-08-30 15:32:08 -0400

    feat: Increase size of PWA icon and update footer text

--- components/DashboardFooter.tsx
+++ components/DashboardFooter.tsx
@@ -2,11 +2,11 @@ import Image from "next/image";
 export const DashboardFooter = () => (
   <footer className="fixed bottom-0 left-0 right-0 bg-base-300 text-center py-2 text-xs z-10">
     <div className="flex items-center justify-center">
-      <p className="mr-2">
+      <p className="mr-2 text-sm">
         Download the app from any browser by clicking the icon in the address
         bar:
       </p>
-      <Image src={"/pwa.png"} width={20} height={20} alt="PWA icon" />
+      <Image src={"/pwa.png"} width={30} height={30} alt="PWA icon" />
     </div>
   </footer>
 );
