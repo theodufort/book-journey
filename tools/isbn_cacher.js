08e8d82e082e69dad0ecdd3d2c91309eb5b1195d
Théo Dufort <theodufort05@gmail.com>
2024-09-05 17:08:31 -0400

    update

--- tools/isbn_cacher.js
+++ tools/isbn_cacher.js
@@ -1,14 +1,23 @@
 import axios from "axios";
+import axiosRetry from "axios-retry";
 import * as fs from "fs";
+axiosRetry(axios, { retries: 5 });
+axiosRetry(axios, {
+  retryDelay: (retryCount) => {
+    return retryCount * 1000;
+  },
+});
 async function main() {
   try {
     const data = fs.readFileSync("./tools/isbn_list.txt", "utf-8");
     const lines = data.trim().split("\n");
 
     for (const line of lines) {
-      console.log("line");
+      console.log(line);
       try {
-        await axios.get(`http://localhost:3000/api/books/${line.trim()}`);
+        await axios.get(
+          `http://localhost:3000/api/books/${line.trim()}?useProxy=true`
+        );
       } catch {}
     }
   } catch (error) {
