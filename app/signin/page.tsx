37894a5d8251c5f5f1f5ff70bda9ccd9628ed8bf
Théo Dufort <theodufort05@gmail.com>
2024-09-05 08:47:28 -0400

    feat: Add full height to sign-in page

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -58,7 +58,7 @@ export default function Login() {
   };
 
   return (
-    <main className="p-8 md:p-24" data-theme={config.colors.theme}>
+    <main className="p-8 md:p-24 h-full" data-theme={config.colors.theme}>
       <div className="text-center mb-4">
         <Link href="/" className="btn btn-ghost btn-sm">
           <svg
@@ -117,7 +117,7 @@ export default function Login() {
           Sign-up with Google
         </button>
 
-        {/* <div className="divider text-xs text-base-content/50 font-medium">
+        <div className="divider text-xs text-base-content/50 font-medium">
           OR
         </div>
 
@@ -145,7 +145,7 @@ export default function Login() {
             )}
             Send Magic Link
           </button>
-        </form> */}
+        </form>
       </div>
     </main>
   );
