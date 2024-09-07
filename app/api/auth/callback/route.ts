b8d6e5c8eb5b6fe0c16c25fdb18d44e5cbc27729
Théo Dufort <theodufort05@gmail.com>
2024-09-07 08:04:22 -0400

    Update route.ts

--- app/api/auth/callback/route.ts
+++ app/api/auth/callback/route.ts
@@ -11,7 +11,7 @@ export const dynamic = "force-dynamic";
 export async function GET(req: NextRequest) {
   const requestUrl = new URL(req.url);
   const code = requestUrl.searchParams.get("code");
-  var userLoggedIn;
+  var userLoggedIn: any = [];
   if (code) {
     const supabase = createRouteHandlerClient({ cookies });
     await supabase.auth.exchangeCodeForSession(code);
