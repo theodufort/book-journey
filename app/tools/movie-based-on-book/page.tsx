b85e3ee313899f9ca9cc4802cd25cb8be1523d3c
Théo Dufort <theodufort05@gmail.com>
2024-09-16 17:38:07 -0400

    push seo

--- app/tools/movie-based-on-book/page.tsx
+++ app/tools/movie-based-on-book/page.tsx
@@ -1,6 +1,33 @@
 "use client";
 import React, { useState } from "react";
 import axios from "axios";
+import { getSEOTags } from "@/libs/seo";
+
+export async function generateMetadata() {
+  return getSEOTags({
+    title: "Find Movies/Books based on Books/Movies",
+    description:
+      "Find movie or book adaptations of your favorite movie or book.",
+    canonicalUrlRelative: `/tools/movie-based-on-book`,
+    extraTags: {
+      openGraph: {
+        title: "Find Movies/Books based on Books/Movies",
+        description:
+          "Find movie or book adaptations of your favorite movie or book.",
+        url: `/tools/movie-based-on-book`,
+        images: [
+          {
+            url: "",
+            width: 1200,
+            height: 660,
+          },
+        ],
+        locale: "en_US",
+        type: "website",
+      },
+    },
+  });
+}
 
 // OMDb API endpoint with API key
 const movieApiEndpoint = "https://omdbapi.com/?apikey=cf4a454b&t="; // Replace with your OMDb API key
