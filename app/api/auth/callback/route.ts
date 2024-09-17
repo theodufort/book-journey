df88580b595be7ebbb8b472ebd5e8c09dac94635
Théo Dufort <theodufort05@gmail.com>
2024-09-17 18:22:03 -0400

    ok

--- app/api/auth/callback/route.ts
+++ app/api/auth/callback/route.ts
@@ -7,31 +7,39 @@ import { WelcomeEmail } from "@/components/WelcomeTemplate";
 
 export const dynamic = "force-dynamic";
 
-// This route is called after a successful login. It exchanges the code for a session and redirects to the callback URL (see config.js).
 export async function GET(req: NextRequest) {
   const requestUrl = new URL(req.url);
   const code = requestUrl.searchParams.get("code");
-  var userLoggedIn: any = [];
+  const supabase = createRouteHandlerClient({ cookies });
+
   if (code) {
-    const supabase = createRouteHandlerClient({ cookies });
+    // Exchange the code for a session
     await supabase.auth.exchangeCodeForSession(code);
+
+    // Retrieve the current user
     const {
       data: { user },
     } = await supabase.auth.getUser();
-    userLoggedIn = user;
-  } else {
-    const resend = new Resend(process.env.RESEND_API_KEY);
-    const { data, error } = await resend.emails.send({
-      from: "welcome@mybookquest.com",
-      to: userLoggedIn == null ? "theodufort05@gmail.com" : userLoggedIn.email,
-      subject: "Welcome to MyBookQuest!",
-      react: WelcomeEmail(),
-    });
-    if (error) {
-      return Response.json({ error });
+
+    if (user) {
+      // Check if this is a new sign-up
+      if (user.aud === "authenticated") {
+        // New user sign-up detected, send a welcome email
+        const resend = new Resend(process.env.RESEND_API_KEY);
+        const { data, error } = await resend.emails.send({
+          from: "welcome@mybookquest.com",
+          to: user.email,
+          subject: "Welcome to MyBookQuest!",
+          react: WelcomeEmail(),
+        });
+
+        if (error) {
+          return NextResponse.json({ error });
+        }
+      }
     }
   }
 
-  // URL to redirect to after sign in process completes
+  // Redirect to the callback URL after the sign-in/sign-up process
   return NextResponse.redirect(requestUrl.origin + config.auth.callbackUrl);
 }
