320724a871eef6923e3565361406f96548468211
Théo Dufort <theodufort05@gmail.com>
2024-09-17 09:19:01 -0400

    ok

--- app/books-like/[...id]/page.tsx
+++ app/books-like/[...id]/page.tsx
@@ -1,5 +1,35 @@
-import BooksLikeClient from './BooksLikeClient';
-
+import { getSEOTags } from "@/libs/seo";
+import BooksLikeClient from "./BooksLikeClient";
+export async function generateMetadata({
+  params,
+}: {
+  params: { id: string[] };
+}) {
+  const fullSlug = params.id.join("/");
+  const isbn = fullSlug.split("-").pop() || "";
+  const decodedTitle = fullSlug.slice(0, -isbn.length - 1).replace(/-/g, " ");
+  return getSEOTags({
+    title: `Find books like ${decodedTitle}`,
+    description: `Find books like ${decodedTitle}`,
+    canonicalUrlRelative: `/books-like/${fullSlug}`,
+    extraTags: {
+      openGraph: {
+        title: `Find books like ${decodedTitle}`,
+        description: `Find books like ${decodedTitle}`,
+        url: `/books-like/${fullSlug}`,
+        images: [
+          {
+            url: "",
+            width: 1200,
+            height: 660,
+          },
+        ],
+        locale: "en_US",
+        type: "website",
+      },
+    },
+  });
+}
 export default function BooksLike({ params }: { params: { id: string[] } }) {
   return <BooksLikeClient params={params} />;
 }
