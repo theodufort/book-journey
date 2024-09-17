64dfcb68636ad6e014098d6a6771e4d6a7cc30a3
Théo Dufort <theodufort05@gmail.com>
2024-09-17 08:19:15 -0400

    feat: improve book card component layout

--- components/BookCard.tsx
+++ components/BookCard.tsx
@@ -21,7 +21,10 @@ const BookCard: React.FC<BookCardProps> = ({ book }) => {
     <div className="card bg-base-100 shadow-xl">
       <figure>
         <Image
-          src={book.data.volumeInfo.imageLinks?.thumbnail || "/placeholder-book-cover.jpg"}
+          src={
+            book.data.volumeInfo.imageLinks?.thumbnail ||
+            "/placeholder-book-cover.jpg"
+          }
           alt={`Cover of ${book.data.volumeInfo.title}`}
           width={150}
           height={225}
@@ -32,7 +35,10 @@ const BookCard: React.FC<BookCardProps> = ({ book }) => {
         <h2 className="card-title">{book.data.volumeInfo.title}</h2>
         <p>{book.data.volumeInfo.authors?.join(", ") || "Unknown Author"}</p>
         <div className="card-actions justify-end">
-          <Link href={`/books-like/${book.isbn_13}`} className="btn btn-primary">
+          <Link
+            href={`/books-like/${book.isbn_13}`}
+            className="btn btn-primary"
+          >
             Find Similar
           </Link>
         </div>
