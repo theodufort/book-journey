aac8f579262d975067294011e4d3a7e8767938f6
Théo Dufort <theodufort05@gmail.com>
2024-09-20 16:30:50 -0400

    try catch

--- app/dashboard/reading-list/add/page.tsx
+++ app/dashboard/reading-list/add/page.tsx
@@ -59,13 +59,15 @@ export default function AddBook() {
       .from("reading_list")
       .select("*", { count: "exact", head: true });
     if (count == 0) {
-      const resend = new Resend(process.env.RESEND_API_KEY);
-      const { data, error } = await resend.emails.send({
-        from: "welcome@mybookquest.com",
-        to: user == null ? "theodufort05@gmail.com" : user.email,
-        subject: "Congrats on adding your first book!",
-        react: FirstBookTemplate(),
-      });
+      try {
+        const resend = new Resend(process.env.RESEND_API_KEY);
+        const { data, error } = await resend.emails.send({
+          from: "welcome@mybookquest.com",
+          to: user == null ? "theodufort05@gmail.com" : user.email,
+          subject: "Congrats on adding your first book!",
+          react: FirstBookTemplate(),
+        });
+      } catch {}
     }
     const { error } = await supabase.from("reading_list").upsert({
       user_id: user.id,
