c27c98f965a7a5ce2b537a1acf386aa8abf8a048
Théo Dufort <theodufort05@gmail.com>
2024-09-27 15:30:12 -0400

    p

---
+++ app/tools/ai-helper/HelperUI.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+function HelperUI() {
+  return (
+    <section>
+      <div className="max-w-7xl mx-auto px-8 py-5 text-center">
+        <h2 className="max-w-3xl mx-auto font-extrabold text-4xl md:text-5xl tracking-tight mb-6 md:mb-8">
+          Find Movies/Books based on Books/Movies
+        </h2>
+      </div>
+      <div></div>
+    </section>
+  );
+}
+
+export default HelperUI;
