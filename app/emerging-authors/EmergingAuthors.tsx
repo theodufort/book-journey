0012100de0c822ee52b818ee09b63a0cdc4754ca
Théo Dufort <theodufort05@gmail.com>
2024-09-28 15:58:07 -0400

    feat: Add EmergingAuthors component and page

---
+++ app/emerging-authors/EmergingAuthors.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+function EmergingAuthors() {
+  return (
+    <section>
+      <div className="max-w-7xl mx-auto px-8 py-5 text-center">
+        <h2 className="max-w-3xl mx-auto font-extrabold text-4xl md:text-5xl tracking-tight">
+          Find Emerging Authors
+        </h2>
+      </div>
+    </section>
+  );
+}
+
+export default EmergingAuthors;
