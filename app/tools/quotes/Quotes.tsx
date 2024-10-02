36143b3eac997563bbcc25f6256182aa1fb8cec7
Théo Dufort <theodufort05@gmail.com>
2024-10-02 19:20:12 -0400

    push

---
+++ app/tools/quotes/Quotes.tsx
@@ -0,0 +1,13 @@
+"use client";
+export default function Quotes() {
+  return (
+    <section>
+      <div className="max-w-7xl mx-auto px-8 py-5 text-center">
+        <h2 className="max-w-3xl mx-auto font-extrabold text-4xl md:text-5xl tracking-tight">
+          Quotes
+        </h2>
+      </div>
+      <div className="card bg-base-200 shadow-xl mt-5 w-full max-w-6xl mx-auto"></div>
+    </section>
+  );
+}
