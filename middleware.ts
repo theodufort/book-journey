4300514ac976e9b999c0cb8f6633625d7f45006d
Théo Dufort <theodufort05@gmail.com>
2024-10-04 08:43:44 -0400

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -4,6 +4,12 @@ import { NextRequest, NextResponse } from "next/server";
 // The middleware is used to refresh the user's session before loading Server Component routes
 export async function middleware(req: NextRequest) {
   const res = NextResponse.next();
+  const url = req.nextUrl;
+  const locale = url.searchParams.get("locale");
+  if (locale) {
+    // Set the NEXT_LOCALE cookie with the value from the URL
+    res.cookies.set("NEXT_LOCALE", locale, { path: "/" });
+  }
   const supabase = createMiddlewareClient({ req, res });
   await supabase.auth.getSession();
   return res;
