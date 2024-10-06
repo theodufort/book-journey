35ef8f502bd313b629dc2f119619cc3394e4790d
Théo Dufort <theodufort05@gmail.com>
2024-10-06 16:11:01 -0400

    modify emal flow

--- app/api/email/templates/FirstBookTemplate.tsx
+++ app/api/email/templates/FirstBookTemplate.tsx
@@ -1,5 +1,3 @@
-import * as React from "react";
-
 export const FirstBookTemplate = () => (
   <div>
     <h1>Congratulations on Adding Your First Book!</h1>
@@ -11,22 +9,6 @@ export const FirstBookTemplate = () => (
     <p>
       This is the first step in curating your personalized book collection. We
       can’t wait to see what other great reads you’ll add next!
-    </p>
-    <p>
-      If you have any questions or need help discovering more books that match
-      your tastes, feel free to reach out or schedule a{" "}
-      <strong>15-minute call</strong>
-      with us. We’d be happy to walk you through the platform or suggest new
-      books for your library.
-    </p>
-    <p>Click the link below to schedule a time:</p>
-    <a href="https://calendly.com/expoweb/15-minutes-onboarding">
-      Schedule a 15-Minute Call
-    </a>
-    <p>
-      As always, you can reach me directly at{" "}
-      <a href="mailto:info@mybookquest.com">info@mybookquest.com</a> with any
-      immediate questions.
     </p>
     <p>
       Keep adding to your collection, and let’s explore even more exciting books
---
+++ app/api/email/templates/FirstPointsFromBooksTemplate.tsx
@@ -0,0 +1,28 @@
+export const FirstPointsFromBooksTemplate = () => (
+  <div>
+    <h1>Congratulations on Earning your First Points!</h1>
+    <p>Hi,</p>
+    <p>
+      We’re excited to let you know that you’ve just earned your fisit points on
+      MyBookQuest! 🎉
+    </p>
+    <p>
+      This is the first step towards getting those juicy discounts to fuel your
+      passion!
+    </p>
+    <p>
+      Keep adding to your collection, review and rate your books to earn more
+      points!
+    </p>
+    <p>
+      Best regards,
+      <br />
+      Theo Dufort
+      <br />
+      Founder, MyBookQuest
+    </p>
+    <p>
+      MyBookQuest | <a href="https://mybookquest.com">Visit our website</a>
+    </p>
+  </div>
+);
