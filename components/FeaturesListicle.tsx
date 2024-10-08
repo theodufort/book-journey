7bda0d7182f1448bfc118537cadc9904417fa862
Théo Dufort <theodufort05@gmail.com>
2024-10-08 13:38:42 -0400

    p

--- components/FeaturesListicle.tsx
+++ components/FeaturesListicle.tsx
@@ -43,22 +43,7 @@ const FeaturesGrid = () => {
           </ul>
         </>
       ),
-      svg: (
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={1.5}
-          stroke="currentColor"
-          className="w-8 h-8"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M2.25 8.25h19.5M2.25 9h19.5m-16.5 5.25h6m-6 2.25h3m-3.75 3h15a2.25 2.25 0 002.25-2.25V6.75A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25v10.5A2.25 2.25 0 004.5 19.5z"
-          />
-        </svg>
-      ),
+      svg: <span className="text-4xl">🎁</span>,
     },
     {
       name: t("featureslisticle_feat2"),
@@ -89,22 +74,7 @@ const FeaturesGrid = () => {
           </ul>
         </>
       ),
-      svg: (
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={1.5}
-          stroke="currentColor"
-          className="w-8 h-8"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z"
-          />
-        </svg>
-      ),
+      svg: <span className="text-4xl">⚖️</span>,
     },
     {
       name: t("featureslisticle_feat3"),
@@ -134,22 +104,91 @@ const FeaturesGrid = () => {
           </ul>
         </>
       ),
-      svg: (
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={1.5}
-          stroke="currentColor"
-          className="w-8 h-8"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m5.231 13.481L15 17.25m-4.5-15H5.625c-.621 0-1.125.504-1.125 1.125v16.5c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9zm3.75 11.625a2.625 2.625 0 11-5.25 0 2.625 2.625 0 015.25 0z"
-          />
-        </svg>
+      svg: <span className="text-4xl">📚</span>,
+    },
+    {
+      name: t("featureslisticle_feat4"),
+      description: (
+        <>
+          <ul className="space-y-2 text-left">
+            {[
+              t("featureslisticle_feat4_sub1"),
+              t("featureslisticle_feat4_sub2"),
+            ].map((item) => (
+              <li key={item} className="flex items-center gap-3 text-left">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                  className="w-[18px] h-[18px] inline shrink-0 opacity-80"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+                {item}
+              </li>
+            ))}
+          </ul>
+        </>
       ),
+      svg: <span className="text-4xl">✍️</span>,
+    },
+    {
+      name: t("featureslisticle_feat5"),
+      description: (
+        <>
+          <ul className="space-y-1 text-left">
+            {[t("featureslisticle_feat5_sub1")].map((item) => (
+              <li key={item} className="flex items-center gap-3 text-left">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                  className="w-[18px] h-[18px] inline shrink-0 opacity-80"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+                {item}
+              </li>
+            ))}
+          </ul>
+        </>
+      ),
+      svg: <span className="text-4xl">📊</span>,
+    },
+    {
+      name: t("featureslisticle_feat6"),
+      description: (
+        <>
+          <ul className="space-y-1 text-left">
+            {[t("featureslisticle_feat6_sub1")].map((item) => (
+              <li key={item} className="flex items-center gap-3 text-left">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                  className="w-[18px] h-[18px] inline shrink-0 opacity-80"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+                {item}
+              </li>
+            ))}
+          </ul>
+        </>
+      ),
+      svg: <span className="text-4xl">📥</span>,
     },
   ];
   return (
