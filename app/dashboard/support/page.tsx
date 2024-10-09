59fa1765d7ede6a211be07fa3161593cd39f76c6
Théo Dufort <theodufort05@gmail.com>
2024-10-09 12:34:54 -0400

    feat: Add dashboard header and update support page layout

--- app/dashboard/support/page.tsx
+++ app/dashboard/support/page.tsx
@@ -1,16 +1,25 @@
-import React from 'react';
-import EmailForm from '@/components/EmailForm';
+"use client";
+import HeaderDashboard from "@/components/DashboardHeader";
+import EmailForm from "@/components/EmailForm";
+import React from "react";
 
 const SupportPage: React.FC = () => {
   return (
-    <div className="container mx-auto px-4 py-8">
-      <h1 className="text-2xl font-bold mb-4">Support</h1>
-      <p className="mb-4">If you need assistance, please fill out the form below:</p>
-      <EmailForm 
-        onSuccess={() => alert('Email sent successfully!')}
-        onError={(error) => alert(`Error sending email: ${error}`)}
-      />
-    </div>
+    <main className="min-h-screen p-8 pb-24">
+      <section className="max-w-6xl mx-auto space-y-8">
+        <div className="z-50">
+          <HeaderDashboard />
+        </div>
+        <h1 className="text-2xl font-bold mb-4">Support</h1>
+        <p className="mb-4">
+          If you need assistance, please fill out the form below:
+        </p>
+        <EmailForm
+          onSuccess={() => alert("Email sent successfully!")}
+          onError={(error) => alert(`Error sending email: ${error}`)}
+        />
+      </section>
+    </main>
   );
 };
 
