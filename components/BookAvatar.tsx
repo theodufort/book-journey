b35c6a46d2dbb16098158fdbb6bc5edf1fd60ca4
Théo Dufort <theodufort05@gmail.com>
2024-10-10 08:38:58 -0400

    p

--- components/BookAvatar.tsx
+++ components/BookAvatar.tsx
@@ -1,5 +1,4 @@
 import { Volume } from "@/interfaces/GoogleAPI";
-import { createAffLink } from "@/libs/amazon-aff";
 import { Database } from "@/types/supabase";
 import {
   createClientComponentClient,
@@ -9,6 +8,7 @@ import { useTranslations } from "next-intl";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { useEffect, useMemo, useState } from "react";
+import ViewSellers from "./ViewSellers";
 
 interface Props {
   vol: Volume;
@@ -132,7 +132,7 @@ const BookAvatar = ({ vol, isBlurred, allowAdd }: Props) => {
             {description.length > MAX_LENGTH && (
               <button
                 onClick={handleToggleExpand}
-                className="text-blue-500 ml-2"
+                className="text-blue-500 md:ml-2"
               >
                 {isExpanded ? "Read Less" : "Read More"}
               </button>
@@ -162,56 +162,9 @@ const BookAvatar = ({ vol, isBlurred, allowAdd }: Props) => {
                   </select>
                 </div>
               )}
-              <div className="dropdown dropdown-top">
-                <div tabIndex={0} role="button" className="btn btn-primary p-2">
-                  {t("view_sellers")}
-                </div>
-                <ul
-                  tabIndex={0}
-                  className="dropdown-content menu bg-base-100 rounded-box z-[1] w-52 p-2 shadow"
-                >
-                  <li>
-                    <a
-                      href={`https://www.anrdoezrs.net/click-101259626-15734439?url=https://booksamillion.com/search2?query=${vol.volumeInfo.title}`}
-                      target="_blank"
-                    >
-                      Books a Million
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      href={`https://www.jdoqocy.com/click-101259626-15736645?url=https%3A%2F%2Fwww.2ndandcharles.com%2Fbook%2Fbrowse%2Fkeyword%2F${vol.volumeInfo.title}`}
-                      target="_blank"
-                    >
-                      2ND and Charles
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      href={`https://www.tkqlhce.com/click-101259626-15734795?url=https%3A%2F%2Fwww.barnesandnoble.com%2Fb%2F${vol.volumeInfo.title}%2F_%2FN-1p38`}
-                      target="_blank"
-                    >
-                      Barnes and Nobles
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      href={`https://www.tkqlhce.com/click-101259626-15733690?url=https%3A%2F%2Fwww.audiobooks.com%2Fsearch%2Fbook%2F${vol.volumeInfo.title}`}
-                      target="_blank"
-                    >
-                      Audiobooks.com
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      href={createAffLink(vol.volumeInfo.title)}
-                      target="_blank"
-                    >
-                      Amazon
-                    </a>
-                  </li>
-                </ul>
-              </div>
+            </div>
+            <div className="mt-2">
+              <ViewSellers title={vol.volumeInfo.title} />
             </div>
           </div>
         </div>
