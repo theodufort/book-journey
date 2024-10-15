e25689a8efa0513fbf97a29ed8df51a3ca052b74
Théo Dufort <theodufort05@gmail.com>
2024-10-15 13:41:13 -0400

    feat: Add logging for cookies in auth callback route

--- app/api/auth/callback/route.ts
+++ app/api/auth/callback/route.ts
@@ -9,7 +9,7 @@ export const dynamic = "force-dynamic";
 export async function GET(req: NextRequest) {
   const requestUrl = new URL(req.url);
   const code = requestUrl.searchParams.get("code");
-
+  console.log(cookies().getAll());
   if (code) {
     const supabase = createRouteHandlerClient({ cookies });
     const {
