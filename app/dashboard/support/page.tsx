e058f57590fb0f356b99a329c75dc553ac00d33c
Théo Dufort <theodufort05@gmail.com>
2024-10-15 07:48:45 -0400

    feat: Add Calendly widget to support page

--- app/dashboard/support/page.tsx
+++ app/dashboard/support/page.tsx
@@ -2,6 +2,7 @@
 import HeaderDashboard from "@/components/DashboardHeader";
 import EmailForm from "@/components/EmailForm";
 import React from "react";
+import { InlineWidget } from "react-calendly";
 
 const SupportPage: React.FC = () => {
   return (
@@ -10,15 +11,22 @@ const SupportPage: React.FC = () => {
         <div className="z-50">
           <HeaderDashboard />
         </div>
-        <div className="p-8 bg-base-200 shadow-md rounded-lg overflow-hidden md:w-1/2 m-auto">
+        <div className="p-8 bg-base-200 shadow-md rounded-lg overflow-hidden m-auto h-full w-auto">
           <h1 className="text-3xl font-bold mb-6">Support</h1>
-          <p className="mb-6">
-            If you need assistance, please fill out the form below:
-          </p>
-          <EmailForm
-            onSuccess={() => alert("Email sent successfully!")}
-            onError={(error) => alert(`Error sending email: ${error}`)}
-          />
+          <div className="grid md:grid-cols-2 gap-4">
+            <div>
+              <p className="mb-6">
+                If you need assistance, please fill out the form below:
+              </p>
+              <EmailForm
+                onSuccess={() => alert("Email sent successfully!")}
+                onError={(error) => alert(`Error sending email: ${error}`)}
+              />
+            </div>
+            <div className="h-auto">
+              <InlineWidget url="https://calendly.com/expoweb/15-minutes-onboarding?hide_event_type_details=1&hide_gdpr_banner=1&background_color=1a1a1a&text_color=ffffff&primary_color=737eff" />
+            </div>
+          </div>
         </div>
       </section>
     </main>
