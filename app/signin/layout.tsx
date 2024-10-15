43e4568eb76747a5d35e8aa2e6ee28529d336d4f
Théo Dufort <theodufort05@gmail.com>
2024-10-15 13:32:59 -0400

    feat: Add referral code handling in signin layout

--- app/signin/layout.tsx
+++ app/signin/layout.tsx
@@ -1,12 +1,19 @@
-import { ReactNode } from "react";
+"use server";
 import config from "@/config";
 import { getSEOTags } from "@/libs/seo";
-
+import { cookies } from "next/headers";
+import { ReactNode } from "react";
 export const metadata = getSEOTags({
   title: `Welcome to ${config.appName}`,
   canonicalUrlRelative: "/auth/signin",
 });
 
 export default function Layout({ children }: { children: ReactNode }) {
+  const ref = searchParams.get("ref");
+  console.log(ref);
+  if (ref) {
+    // Store the ref code in localStorage
+    cookies().set("referralCode", ref);
+  }
   return <>{children}</>;
 }
