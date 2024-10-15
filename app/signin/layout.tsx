899ee2a712cb90f3730845b54e305f343aed7fd9
Théo Dufort <theodufort05@gmail.com>
2024-10-15 13:35:18 -0400

    fix: Remove "use server" directive from signin layout

--- app/signin/layout.tsx
+++ app/signin/layout.tsx
@@ -1,14 +1,12 @@
-"use server";
 import config from "@/config";
 import { getSEOTags } from "@/libs/seo";
-import { cookies } from "next/headers";
 import { ReactNode } from "react";
 export const metadata = getSEOTags({
   title: `Welcome to ${config.appName}`,
   canonicalUrlRelative: "/auth/signin",
 });
 
-export default function Layout({ children }: { children: ReactNode }) {
+export default async function Layout({ children }: { children: ReactNode }) {
   return <>{children}</>;
 }
 
@@ -16,7 +14,10 @@ export async function getServerSideProps(context) {
   const { ref } = context.query;
   if (ref) {
     // Set the referral code as a cookie
-    context.res.setHeader('Set-Cookie', `referralCode=${ref}; Path=/; HttpOnly`);
+    context.res.setHeader(
+      "Set-Cookie",
+      `referralCode=${ref}; Path=/; HttpOnly`
+    );
   }
   return { props: {} };
 }
