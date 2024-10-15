8bce325eb1612919e95f8ed252ada7fdf6da6f60
Théo Dufort <theodufort05@gmail.com>
2024-10-15 07:51:50 -0400

    p

--- app/dashboard/support/page.tsx
+++ app/dashboard/support/page.tsx
@@ -11,7 +11,7 @@ const SupportPage: React.FC = () => {
         <div className="z-50">
           <HeaderDashboard />
         </div>
-        <div className="p-4 sm:p-8 bg-base-200 shadow-md rounded-lg overflow-hidden m-auto w-auto">
+        <div className="p-4 sm:p-8 bg-base-200 shadow-md rounded-lg m-auto w-auto">
           <h1 className="text-2xl sm:text-3xl font-bold mb-6">Support</h1>
           <div className="space-y-8 lg:space-y-0 lg:grid lg:grid-cols-2 lg:gap-8">
             <div className="space-y-6">
@@ -26,7 +26,7 @@ const SupportPage: React.FC = () => {
             <div className="h-[500px] sm:h-[600px] md:h-[700px] lg:h-[600px]">
               <InlineWidget
                 url="https://calendly.com/expoweb/15-minutes-onboarding?hide_event_type_details=1&hide_gdpr_banner=1&background_color=1a1a1a&text_color=ffffff&primary_color=737eff"
-                styles={{ height: '100%', width: '100%' }}
+                styles={{ height: "100%", width: "100%" }}
               />
             </div>
           </div>
