68926eb05d5a3a913f81f7eb53c6e31a673b5f22
Théo Dufort <theodufort05@gmail.com>
2024-10-15 14:02:54 -0400

    p

--- app/signin/layout.tsx
+++ app/signin/layout.tsx
@@ -1,6 +1,5 @@
 import config from "@/config";
 import { getSEOTags } from "@/libs/seo";
-import { cookies } from "next/headers";
 import { ReactNode } from "react";
 
 export const metadata = getSEOTags({
@@ -9,14 +8,5 @@ export const metadata = getSEOTags({
 });
 
 export default async function Layout({ children }: { children: ReactNode }) {
-  const cookieStore = cookies();
-  const ref = cookieStore.get("referralCode")?.value;
-
-  if (ref) {
-    // The cookie is set by the page component, we can use it here if needed
-    console.log("Referral code from cookie:", ref);
-    // You can add any server-side logic here that needs to use the referral code
-  }
-
   return <>{children}</>;
 }
