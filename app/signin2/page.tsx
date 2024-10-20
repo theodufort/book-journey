fae372d59079df678b3d82c2634069e93968e24d
Théo Dufort <theodufort05@gmail.com>
2024-10-20 14:39:32 -0400

    p

--- app/signin2/page.tsx
+++ app/signin2/page.tsx
@@ -10,6 +10,21 @@ import { useEffect, useState } from "react";
 import toast from "react-hot-toast";
 
 export default function Login() {
+  useEffect(() => {
+    supabase.auth.onAuthStateChange(async (event, session) => {
+      if (event == "PASSWORD_RECOVERY") {
+        const newPassword = prompt(
+          "What would you like your new password to be?"
+        );
+        const { data, error } = await supabase.auth.updateUser({
+          password: newPassword,
+        });
+
+        if (data) alert("Password updated successfully!");
+        if (error) alert("There was an error updating your password.");
+      }
+    });
+  }, []);
   const router = useRouter();
   const supabase = createClientComponentClient<Database>();
   const [isLoading, setIsLoading] = useState<boolean>(false);
@@ -144,21 +159,7 @@ export default function Login() {
        * Step 2: Once the user is redirected back to your application,
        * ask the user to reset their password.
        */
-      useEffect(() => {
-        supabase.auth.onAuthStateChange(async (event, session) => {
-          if (event == "PASSWORD_RECOVERY") {
-            const newPassword = prompt(
-              "What would you like your new password to be?"
-            );
-            const { data, error } = await supabase.auth.updateUser({
-              password: newPassword,
-            });
 
-            if (data) alert("Password updated successfully!");
-            if (error) alert("There was an error updating your password.");
-          }
-        });
-      }, []);
       if (error) throw error;
       toast.success("Password reset email sent. Please check your inbox.");
       setIsResetMode(false);
