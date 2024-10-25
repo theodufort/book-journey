8242574e5f4c9c76fdf3495fe1a4f903de934151
Théo Dufort <theodufort05@gmail.com>
2024-10-25 15:43:49 -0400

    add login tracking

---
+++ app/dashboard/book-nook/page.tsx
@@ -0,0 +1,13 @@
+import HeaderDashboard from "@/components/DashboardHeader";
+
+export default function BookNook() {
+  return (
+    <main className="min-h-screen p-4 sm:p-8 pb-16">
+      <section className="max-w-6xl mx-auto space-y-4 sm:space-y-8">
+        <div className="sticky top-0 z-50 bg-base-100">
+          <HeaderDashboard />
+        </div>
+      </section>
+    </main>
+  );
+}
