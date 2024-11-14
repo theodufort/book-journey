85fec07adc7d05957ed3cb1267c898884a3722a1
Théo Dufort <theodufort05@gmail.com>
2024-11-13 19:56:11 -0500

    p

--- config.ts
+++ config.ts
@@ -27,40 +27,17 @@ const config = {
         //  REQUIRED - Name of the plan, displayed on the pricing page
         name: "Starter",
         // A friendly description of the plan, displayed on the pricing page. Tip: explain why this plan and not others
-        description: "Perfect for small projects",
+        description: "Get started with your second brain.",
         // The price you want to display, the one user will be charged on Stripe.
         price: 99,
         // If you have an anchor price (i.e. $29) that you want to display crossed out, put it here. Otherwise, leave it empty
         priceAnchor: 149,
         features: [
           {
-            name: "NextJS boilerplate",
+            name: "Vocal notes feature",
           },
-          { name: "User oauth" },
-          { name: "Database" },
-          { name: "Emails" },
-        ],
-      },
-      {
-        priceId:
-          process.env.NODE_ENV === "development"
-            ? "price_1O5KtcAxyNprDp7iftKnrrpw"
-            : "price_456",
-        // This plan will look different on the pricing page, it will be highlighted. You can only have one plan with isFeatured: true
-        isFeatured: true,
-        name: "Advanced",
-        description: "You need more power",
-        price: 149,
-        priceAnchor: 299,
-        features: [
-          {
-            name: "NextJS boilerplate",
-          },
-          { name: "User oauth" },
-          { name: "Database" },
-          { name: "Emails" },
-          { name: "1 year of updates" },
-          { name: "24/7 support" },
+          { name: "Unlimited vocal notes storage" },
+          { name: "Unlimited vocal to text conversions" },
         ],
       },
     ],
