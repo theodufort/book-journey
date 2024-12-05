5a505ff6e4d71a33ecac8a8b79782e259bfb7b1a
Théo Dufort <theodufort05@gmail.com>
2024-12-05 18:05:10 -0500

    feat: Add getTourFinished method to conditionally render onboarding tour

--- app/dashboard/NextStepWrapper.tsx
+++ app/dashboard/NextStepWrapper.tsx
@@ -18,25 +18,37 @@ export default function NextStepWrapper({
   const setTourFinished = async () => {
     // Get the tour name from the first step group
     const tourName = steps[0]?.tour || "default";
-    
+
     const { data, error } = await supabase
       .from("onboarding")
       .upsert({ user_id: userId, onboarded: true, tour_name: tourName })
       .eq("user_id", userId);
   };
+  const getTourFinished = async () => {
+    // Get the tour name from the first step group
+    const tourName = steps[0]?.tour || "default";
 
+    const { data: dataOnboarding, error } = await supabase
+      .from("onboarding")
+      .select("onboarded, tour_name")
+      .eq("user_id", userId)
+      .single();
+    return dataOnboarding.onboarded;
+  };
   return (
     <NextStepProvider>
-      <NextStep
-        cardComponent={OnboardingCard}
-        onComplete={setTourFinished}
-        steps={steps}
-        shadowRgb="0, 0, 0"
-        shadowOpacity="0.8"
-        cardTransition={{ duration: 0.2, type: "spring" }}
-      >
-        {children}
-      </NextStep>
+      {getTourFinished() ? (
+        <NextStep
+          cardComponent={OnboardingCard}
+          onComplete={setTourFinished}
+          steps={steps}
+          shadowRgb="0, 0, 0"
+          shadowOpacity="0.8"
+          cardTransition={{ duration: 0.2, type: "spring" }}
+        >
+          {children}
+        </NextStep>
+      ) : null}
     </NextStepProvider>
   );
 }
