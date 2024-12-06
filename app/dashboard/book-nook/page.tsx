cf2c62d82312f23a9ac3f12e353fb8c22700483d
Théo Dufort <theodufort05@gmail.com>
2024-12-06 12:48:43 -0500

    push onboarding + fix transcribe

--- app/dashboard/book-nook/page.tsx
+++ app/dashboard/book-nook/page.tsx
@@ -1,8 +1,15 @@
 "use client";
 import BookNook2 from "@/components/BookNook2";
 import HeaderDashboard from "@/components/DashboardHeader";
+import { useNextStep } from "nextstepjs";
 
 export default function BookNook() {
+  const { startNextStep } = useNextStep();
+
+  const handleStartTour = () => {
+    startNextStep("booknookTour");
+  };
+  handleStartTour();
   return (
     <main className="min-h-screen p-4 sm:p-8 pb-16">
       <section className="mx-auto space-y-4 sm:space-y-8">
